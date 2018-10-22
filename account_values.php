621b4bb0890283cfab5f71e3eb5dfad9aa95c8eb
j311yb311y97 <38288407+j311yb311y97@users.noreply.github.com>
2018-10-22 13:41:46 -0500

    trying to get input working
    Getting input to work with php. Have to put on server to get to work.

--- account_values.php
+++ account_values.php
@@ -1,6 +1,20 @@
 <html>
   <title> Info here </title>
   <body>
+    <?php
+       $servername = "localhost";
+       $username = "fvres";
+       $password = "password";
+
+       // Create connection
+       $conn = mysqli_connect($servername, $username, $password);
+
+       // Check connection
+       if (!$conn) {
+           die("Connection failed: " . mysqli_connect_error());
+       }
+       echo "Connected successfully";
+    ?>
     <table>
         <?php
             foreach ($_POST as $key => $value) {
@@ -15,5 +29,26 @@
             }
         ?>
     </table>
+    <?php
+
+      $password = $_POST['password'];
+      $first_name = $_POST['first_name'];
+      $last_name = $_POST['last_name'];
+      $accountname = $first_name + " " + $last_name;
+      $company = $_POST['company'];
+      $email = $_POST['email'];
+      $account_type = $_POST['account_type'];
+      $zipcode = $_POST['zipcode'];
+      $cellnumber = $_POST['cellnumber'];
+      $homenumber = $_POST['homenumber'];
+      $banknumber = $_POST['banknumber'];
+      $routingnumber = $_POST['routingnumber'];
+
+
+      $sql = "INSERT INTO accounts(accountname, password, first_name, last_name,
+        company, email, account_type, zipcode, cellnumber, homenumber, banknumber, routingnumber)
+        VALUES ($accountname, $password, $first_name, $last_name,
+          $company, $email, $account_type, $zipcode, $cellnumber, $homenumber, $banknumber, $routingnumber)";
+     ?>
   </body>
 </html>
